4e0c54fc389797e373142cc30f4773705cd30aac
vicky <tyagivivek528@gmail.com>
2024-03-15 19:16:10 +0530

    Changed some code

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
+const express = require("express");
+const app = express();
+const http = require("http");
 const WebSocket = require("ws");
-
-const server = new WebSocket.Server({port: 8181});
-
-server.on("connection", socket => {
-  console.log("Connection established🚀🚀");
-
-  socket.on("message", message => {
-    let msg = message.toString();
-    if (msg === "Connection Req from Dapp") {
-      //Sends notification to bitwallet app frontend to let user approve or deny the request
-
-      let isApprove = false;
-      //if request accepted, sends the crednetials
-      if (isApprove) socket.send(JSON.stringify(credentials));
-      //if request denied, send the message "User denied the request"
-      else socket.send("User denied the request");
-    }
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+wss.on("connection", function connection(ws) {
+  console.log("APP connected!!🚀🌿");
+  ws.on("message", function incoming(message, isBinary) {
+    console.log(message.toString(), isBinary);
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
   });
+});
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+server.listen(8080, () => {
+  console.log("Listening to port 8080");
 });
